6d825ae1a99e3f3cf700895d619de912c32e0448
RealmDragon <realmdragon2014@yandex.ru>
2025-02-06 11:25:03 +0300

    Обновления от 06.02.2025

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from config import views  # Добавляем импорт views
+from main import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
