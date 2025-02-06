0caaac8a18a25014139599484a16c296f8587882
RealmDragon <realmdragon2014@yandex.ru>
2025-02-06 10:58:36 +0300

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from Medical_Site.app.main import views  # Добавляем импорт views
+from config import views  # Добавляем импорт views  # Добавляем импорт views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
